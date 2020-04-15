3ef08c06368752f8f8c77096ca09c076cf5489a8
sgeetha2 <sgeetha2@its.jnj.com>
2020-04-15 15:29:33 +0100

    final commit

--- app/src/main/java/com/sree/helloworldapplication/MainActivity.java
+++ app/src/main/java/com/sree/helloworldapplication/MainActivity.java
@@ -1,14 +1,36 @@
 package com.sree.helloworldapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetingView = (TextView) findViewById(R.id.greeting);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.action_bye){
+            greetingView.setText(R.string.say_bye);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return true;
     }
 }
